b08cb544f04072a8fcdbface6cc620aa069b71ec
Tejas SP <tejasprasad2003@gmail.com>
2020-11-24 19:00:08 +0530

    landing and first page done

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="index.css" />
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>PRAJASEVE</title>
+</head>
+
+<body>
+    <div class="productmain">
+        <div class="mainheading">
+            <h4>
+                <strong>Water Pump Selection 
+                    <br> ನೀರಿನ ಪಂಪ್ ಆಯ್ಕೆ</strong>
+            </h4>
+        </div>
+
+        <div class="container">
+            <div class="categoryinfo">
+                <div class="category_heading">
+                    <h5>Domestic and Community <br> ಗೃಹ ಮತ್ತು ವಸತಿ ನಿಲಯಕ್ಕೆ </h5>
+                </div>
+                <div class="list-group">
+                    <a href="soborewell.php" class="list-group-item">Sump and Open Borewell / ತೊಟ್ಟಿ ಮತ್ತು ತೆರೆದ ಭಾವಿ </a>
+                    <a href="#" class="list-group-item">Bore Well / ಕೊಳವೆ ಭಾವಿ </a>
+                </div>
+            </div>
+            <div class="categoryinfo">
+                <div class="category_heading">
+                    <h5>Agriculture <br> ವ್ಯವಸಾಯಕ್ಕೆ </h5>
+                </div>
+                <div class="list-group">
+                    <a href="#" class="list-group-item">Open Well / ತೆರೆದ ಭಾವಿ </a>
+                    <a href="#" class="list-group-item">Bore Well / ಕೊಳವೆ ಭಾವಿ </a>
+                    <a href="#" class="list-group-item">Canal or River / ಕಾಲುವೆ ಅಥವಾ ನದಿ </a>
+                    <a href="#" class="list-group-item">Lift Irrigation / ಏತ್ ನೀರಾವರಿ </a>
+                    <a href="#" class="list-group-item">Solar Pumps / ಸೌರಶಕ್ತಿ ಪಂಪುಗಳು </a>
+                </div>
+            </div>
+            <div class="categoryinfo">
+                <div class="category_heading">
+                    <h5>Industry <br> ಕೈಗಾರಿಕೆಗೆ </h5>
+                </div>
+                <div class="list-group">
+                    <a href="#" class="list-group-item">Polder / ತೊಟ್ಟಿ ಅಥವಾ ಹೊಂಡದ (ಕೊಳ)</a>
+                    <a href="#" class="list-group-item">Special Purpose Pumps / ವಿಶೇಷ ಉದ್ದೇಶದ ಪಂಪುಗಳು </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ soborewell.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="index.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>PRAJASEVE</title>
+</head>
+
+<body>
+    <div class="productmain">
+        <div class="mainheading">
+            <h5>
+                <strong>Domestic Sump / Open Well Pump Selection  
+                    <br> ಗೃಹ ಮತ್ತು ವಸತಿ ನಿಲಯಕ್ಕೆ ತೊಟ್ಟಿ / ತೆರೆದ ಭಾವಿ ಪಂಪಿನ ಆಯ್ಕೆ  </strong>
+            </h5>
+        </div>
+
+        <div class="container">
+            <form action="soborewell.php">
+                <div class="forminput">
+                    <h5>ನೀರಿನ ಅವಶ್ಯಕತೆ ದಿನಕ್ಕೆ ಲೀಟರ್ಗಳಲ್ಲಿ <br>Water Required per day (in litres) </h5> &emsp;
+                    <input type="number" placeholder="Litres" class="" >
+                </div>
+                
+                <div class="forminput">
+                    <h5>ಒಟ್ಟು ಎಷ್ಟು ಎತ್ತರಕ್ಕೆ ನೀರನ್ನು ಎತ್ತಬೇಕು ಮೀಟರ್ಗಳಲ್ಲಿ <br>Total Height to be pumped(in metres) </h5> &emsp;
+                    <input type="number" placeholder="Metres" class="">
+                </div>
+                
+                <div class="formradio">
+                    <h4>ನೀರಿನ ಅವಶ್ಯಕತೆ ದಿನಕ್ಕೆ ಲೀಟರ್ಗಳಲ್ಲಿ <br>Power Supply available </h4> &emsp;
+                    <div class="radioinput">
+                    <input type="radio" name="power" class="" /> <span>ಒಂದು ಫೇಸ್ ೨೩೦ ವೋಲ್ಟ್ <br> Single Phase 230V</span>
+
+                    </div>
+                    <div class="radioinput">
+                    <input type="radio" name="power" class="" /> <span>ಮೂರು ಫೇಸ್ ೪೧೫ ವೋಲ್ಟ್  <br>Three Phase 415V</span>
+
+                    </div>
+                    
+                </div>
+                <div class="formwaterpump">
+                    <h5>ಒಟ್ಟು ಎಷ್ಟು ಎತ್ತರಕ್ಕೆ ನೀರನ್ನು ಎತ್ತಬೇಕು ಮೀಟರ್ಗಳಲ್ಲಿ <br>Which Make Pump Required</h5> &emsp;
+                    <select type="number" placeholder="Metres" class="">
+                        <option value="">
+                            Samsung
+                        </option>
+                        <option value="">
+                            Realme
+                        </option>
+                        </select>
+                </div>
+                <div class="buttons">
+                   <button type="button" class="btn btn-light" id="clearbtn">CLEAR</button> 
+                   <button type="button" class="btn btn-light" id="searchbtn">SEARCH</button> 
+                </div>
+                
+
+            </form>
+        </div>  
+    </div>
+</body>
+
+</html>
